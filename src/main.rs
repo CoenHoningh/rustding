a30c4ab65b3224575c7198dfe3ec591510670e73
Coen Honingh <coenhoningh@gmail.com>
2024-09-15 00:03:04 +0200

    a

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use argh::FromArgs;
 
-#[derive(FromArgs, Debug)]
+#[derive(FromArgs)]
 /// Dit is een test
 struct Ding {
     /// dit is een optie
@@ -10,11 +10,22 @@ struct Ding {
     /// een array met 10 items
     #[argh(option)]
     grootte: String,
+
+    #[argh(option, from_str_fn(always_five))]
+    /// dit is altijd vijf
+    vijf: u8,
 }
 
 fn main() {
     println!("Hello, world!");
     let test: Ding = argh::from_env();
-    println!("test: {test:?}");
-    println!("test2: {}, {}", test.banaan, test.grootte);
+    let test2 = test.grootte.leak();
+    println!("test: {test2}");
+    println!("test2: {}, {}, {}, {}", test.banaan, 'a', 'b', test.vijf);
+    let _ולפתע: u128 = 987654324;
+    let _bla: &str = " דג סקרן שט בים מאוכזב ולפתע מצא לו חברה איך הקליטה";
+}
+
+fn always_five(_value: &str) -> Result<u8, String> {
+    Ok(5)
 }
