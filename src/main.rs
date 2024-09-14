ab756a5dd737f244b5d6fec7f4d6fd45e6e27112
Coen Honingh <coenhoningh@gmail.com>
2024-09-14 20:27:40 +0200

    yo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use argh::FromArgs;
+
+#[derive(FromArgs, Debug)]
+/// Dit is een test
+struct Ding {
+    /// dit is een optie
+    #[argh(switch, short = 'j')]
+    banaan: bool,
+
+    /// een array met 10 items
+    #[argh(option)]
+    grootte: String,
+}
+
 fn main() {
     println!("Hello, world!");
+    let test: Ding = argh::from_env();
+    println!("test: {test:?}");
+    println!("test2: {}, {}", test.banaan, test.grootte);
 }
