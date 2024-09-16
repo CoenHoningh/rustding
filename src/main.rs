ba095366892875c142098e0e80eb0471244b6918
Coen Honingh <coenhoningh@gmail.com>
2024-09-16 23:05:42 +0200

    memory

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,22 @@
-use std::{borrow::Borrow, collections::LinkedList};
-
 use argh::FromArgs;
+use std::iter::repeat_with;
 
 #[derive(FromArgs, PartialEq)]
 /// Dit is een test
 struct Ding {
     /// getal 1
     #[argh(positional)]
-    first: i32,
+    first: usize,
 
     /// getal 2
     #[argh(positional)]
-    second: i32,
-
-    /// dit is een optie
-    #[argh(switch, short = 'j')]
-    banaan: bool,
+    second: usize,
 }
 
 fn main() {
-    println!("Hello, world!");
-    let test: Ding = argh::from_env();
-
-    if test.banaan {
-        println!("{}", test.first * test.second);
-    } else {
-        let division: f64 = Into::<f64>::into(test.first) / Into::<f64>::into(test.second);
-        println!("{division:.20}");
-    }
-    let lijst = LinkedList::<i64>::from_iter(0..100);
-    let lijstbox = Box::new(lijst.borrow());
-    let lijstaddr = &lijst as *const LinkedList<i64>;
-    let lijstboxaddr = &lijstbox as *const Box<&LinkedList<i64>>;
-    println!("{:p}", lijstaddr);
-    println!("{:p}", &lijstbox);
-    println!("{:#?}", lijstboxaddr);
-    println!("{}", lijstboxaddr as usize - lijstaddr as usize);
-    let _ולפתע: u128 = 987654324;
-    let _bla: &str = " דג סקרן שט בים מאוכזב ולפתע מצא לו חברה איך הקליטה";
+    let Ding { first, second } = argh::from_env();
+    let mut rng = fastrand::Rng::new();
+    let mut subgen = move || repeat_with(|| rng.f64()).take(first).collect::<Vec<f64>>();
+    let matrix: Vec<Vec<f64>> = repeat_with(|| subgen()).take(second).collect();
+    eprintln!("{}", matrix.len());
 }
